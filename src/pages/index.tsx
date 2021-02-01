273f01c34534c3b94df7c9f1c2171ef272919475
Mathis Le Bonniec <mathislbonniec@gmail.com>
2021-02-01 20:38:50 +0100

    feat: add landing page hero section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,42 @@
-import { ReactElement } from 'react';
-import { Editor } from '../components/Highlight';
+import { createElement, FC, ReactElement } from 'react';
+// import { Editor } from '../components/Highlight';
+import cls from '../utils/multi-classes';
 import stl from '../styles/pages/index.module.scss';
+import * as Frameworks from '../assets/frameworks';
+import Arrow from '../assets/arrow.svg';
+import Link from 'next/link';
+
+interface IFrameworks {
+  [key: string]: FC;
+}
 
 export default function Index(): ReactElement {
   return (
-    <main id={stl.homepage} className={'layout'}>
-      <div id={stl.hero}>
-        <div>
-          <h1 className={stl.title}>Dynamically host<br />your SPA images</h1>
-          <p>Host your image to display them on your Single Page Application.</p>
+    <main id={stl.homepage}>
+      <div id={stl.hero} className={'layout'}>
+        <div className={stl.content}>
+          <h1 className={stl.title}>Host your<br /><span className={stl.highlight}>static</span> contents</h1>
+          <p className={stl.subtitle}><b>Bild</b> allows you to host your web or application static contents.</p>
         </div>
-        <div>
-          {/* <Editor /> */}
+        <ul className={cls('flex--justify--center flex--center', stl.frameworks)}>
+          {Object.keys(Frameworks).map(e => (
+            <li key={e}>
+              {createElement((Frameworks as IFrameworks)[e])}
+            </li>
+          ))}
+        </ul>
+        <div className={stl['sign-buttons']}>
+          <Link href="/auth/sign-up">
+            <a className="button--dark">Sign Up For Free<Arrow /></a>
+          </Link>
+          <Link href="/auth/sign-in">
+            <a className={stl['sign-in']}>Sign In<Arrow /></a>
+          </Link>
         </div>
+      </div>
+      <div id={stl.demo} className={'layout'}>
+        {/* <img src="/index/hero.png" alt="Hero Image" /> */}
+        <h1>Hello world...</h1>
       </div>
     </main>
   )
