f07293c6ce8ce652dda6d38648a75218776a298c
Mathis Le Bonniec <mathislbonniec@gmail.com>
2021-02-07 17:31:26 +0100

    fix: add viewport meta to head

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,10 +5,14 @@ import Header from '../components/Header';
 // Styles
 import '../styles/globals.scss';
 import '../styles/components/scribble.scss';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Fragment>
+      <Head>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Header />
       <Component {...pageProps} />
     </Fragment>
