075f8e40478a633fc9949249c2abe34e372fcdb1
Mathis Le Bonniec <mathislbonniec@gmail.com>
2021-02-12 21:53:37 +0100

    docs: add tsdoc to multi-classes function

--- src/utils/multi-classes.ts
+++ src/utils/multi-classes.ts
@@ -1,3 +1,10 @@
+/**
+ * Returns a class string
+ * @remarks
+ * This function permit to you to join strings, variables, null, undefind into one string
+ * @param classes - You classes in strings or in variables
+ * @returns A string that contains all classes
+ */
 export default function multiClasses(...classes: (string | null | undefined)[]): string {
 	const filtered: string[] = classes.filter((e): e is string => typeof e === 'string');
 	
