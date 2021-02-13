32bc10bd21350dd6ae271b2600c8a7b5870a0789
Mathis Le Bonniec <mathislbonniec@gmail.com>
2021-02-13 15:28:04 +0100

    perf: use loops to optimise footer links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,68 +1,93 @@
-import Link from 'next/link';
 import { createElement, ReactElement } from 'react';
-import stl from '../styles/layout/footer.module.scss';
+import Link from 'next/link';
 import cls from '../utils/multi-classes';
-import * as Networks from '../assets/global/networks/index';
+
+// Assets and style
+import { Twitter, Instagram, Facebook, Email } from '../assets/global/networks/index';
+import stl from '../styles/layout/footer.module.scss';
+
+type TPage = [name: string, route: `/${string}`];
+interface ILinks {
+  Product: TPage[];
+  Account: TPage[];
+  Developers: TPage[];
+	Company: TPage[];
+}
 
 export default function Footer(): ReactElement {
+	const networks: [any, string][] = [[Twitter, 'https://twitter.com/bild'], [Instagram, 'https://instagram.com/bild'], [Facebook,  'https://facebook.com/bild'], [Email, 'mailto:contact@bild.com']];
+	const links: ILinks = {
+		'Product': [
+			['Overview', '/overview'],
+			['Pricing', '/pricing'],
+			['Features', '/features']
+		],
+		'Account': [
+			['Dashboard', '/app/dashboard'],
+			['Sign In', '/auth/sign-in'],
+			['Sign Up', '/auth/sign-up']
+		],
+		'Developers': [
+			['Documentation', '/developer/documentation'],
+			['SDK', '/developer/sdk'],
+		],
+		'Company': [
+			['About Us', '/about'],
+			['Contact Us', '/contact'],
+			['Term Of Use', '/term-of-use'],
+			['Support', '/support'],
+		]
+	};
+
 	return (
-		<footer id={stl.footer} className="layout flex--column">
-			<div className={cls('flex', stl.content)}>
-				<div className={stl.brand}>
-					<Link href="/">
-						<a className={stl.brand__logo}>
-							<img src="/images/global/logo.svg" alt="Temporary logo" />
-						</a>
-					</Link>
-					<p className={stl.brand__slogan}><b>Bild</b> allows you to host and manage static content for your websites or application.</p>
-					<ul className={stl.brand__networks}>
-						{Object.keys(Networks).map(e => (
-							<li key={e}>
-								<a href="">
-									{createElement((Networks as SVG)[e])}
-								</a>
-							</li>
-						))}
-					</ul>
-				</div>
-				<div className={cls('flex', stl.links)}>
-					<div className={stl.links__list}>
-						<h5 className={stl.links__title}>Product</h5>
-						<ul>
-							<li>Overview</li>
-							<li>Pricing</li>
-							<li>Features</li>
+		<footer id={stl.footer} className="flex--column">
+			<div className="layout">
+				<div className={cls('flex', stl.content)}>
+					<div className={stl.brand}>
+						<Link href="/">
+							<a className={stl.brand__logo}>
+								<img src="/images/global/logo.svg" alt="Temporary logo" />
+							</a>
+						</Link>
+						<p className={stl.brand__slogan}><b>Bild</b> allows you to host and manage static content for your websites or application.</p>
+						<ul className={stl.brand__networks}>
+							{networks.map((e, i) => (
+								<li key={i}>
+									<a href={e[1]} target="_blank">
+										{createElement(e[0])}
+									</a>
+								</li>
+							))}
 						</ul>
 					</div>
-					<div className={stl.links__list}>
-						<h5 className={stl.links__title}>Account</h5>
-						<ul>
-							<li>Dashboard</li>
-							<li>Sign In</li>
-							<li>Sign Up</li>
-						</ul>
-					</div>
-					<div className={stl.links__list}>
-						<h5 className={stl.links__title}>Developers</h5>
-						<ul>
-							<li>Documentation</li>
-							<li>SDK</li>
-						</ul>
-					</div>
-					<div className={stl.links__list}>
-						<h5 className={stl.links__title}>Company</h5>
-						<ul>
-							<li>About Us</li>
-							<li>Contact Us</li>
-							<li>Term Of Use</li>
-						</ul>
+					<div className={cls('flex', stl.links)}>
+						{Object.entries(links).map((e) => {
+							const [title, pages] = e;
+							return (
+								<div className={stl.links__list} key={title.toLowerCase()}>
+									<h5 className={stl.links__title}>{title}</h5>
+									<ul>
+										{pages.map((e: TPage) => {
+											const [name, route] = e;
+											return (
+												<li>
+													<Link href={route}>
+														<a>{name}</a>
+													</Link>
+												</li>
+											);
+										})}
+									</ul>
+								</div>
+							);
+						})}
 					</div>
 				</div>
-			</div>
-			<div className={cls('flex--center', stl.mentions)}>
-				<p>&copy; 2021 Bild.</p>
-				<p className={stl.mentions__made}>Made with <span className={stl.mentions__made__heart}>♥</span> from France</p>
-				<p>All rights reserved.</p>
+				<div className={cls('flex--center', stl.mentions)}>
+					<p>&copy; 2021 Bild.</p>
+					<p className={stl.mentions__made}>Made with <span className={stl.mentions__made__heart}>♥</span> from France</p>
+					<p>All rights reserved.</p>
+				</div>
 			</div>
 		</footer>
 	);
